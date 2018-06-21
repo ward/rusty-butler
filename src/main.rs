5b80836b0243351ea375f82c3fec1a8662274cbb
Ward Muylaert <ward.muylaert@gmail.com>
2018-06-21 08:40:44 +0200

    Add irc crate and their basic example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate irc;
+
+use irc::client::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::load("bot.toml").expect("Failed to load config");
+    let mut reactor = IrcReactor::new().unwrap();
+    let client = reactor.prepare_client_and_connect(&config)
+        .expect("Failed to create client");
+    client.identify().expect("Failed to identify");
+    reactor.register_client_with_handler(client, |client, irc_msg| {
+        println!("{}", irc_msg);
+        if let Command::PRIVMSG(channel, message) = irc_msg.command {
+            if message.contains(client.current_nickname()) {
+                // send_privmsg comes from ClientExt
+                client.send_privmsg(&channel, "beep boop").unwrap();
+            }
+        }
+        Ok(())
+    });
+    reactor.run().unwrap();
 }
