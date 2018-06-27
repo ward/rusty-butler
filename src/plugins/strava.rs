71e857371e5275e70e87199a485fc973c68837ff
Ward Muylaert <ward.muylaert@gmail.com>
2018-06-27 21:53:07 +0200

    Actually pass access_token around +finish activity

--- src/plugins/strava.rs
+++ src/plugins/strava.rs
@@ -8,13 +8,14 @@ pub fn handler(client: &IrcClient, msg: &Message, config: &Config) {
     if access_token.is_none() {
         return ()
     }
+    let access_token = access_token.unwrap();
     if let Command::PRIVMSG(ref channel, ref message) = msg.command {
-        let segment_reply = handle_segments(message);
+        let segment_reply = handle_segments(message, &access_token);
         match segment_reply {
             Some(segment_id) => client.send_privmsg(&channel, &segment_id).unwrap(),
             _ => (),
         }
-        let activity_reply = handle_activities(message);
+        let activity_reply = handle_activities(message, &access_token);
         match activity_reply {
             Some(reply) => client.send_privmsg(&channel, &reply).unwrap(),
             _ => (),
@@ -30,10 +31,10 @@ pub fn get_access_token(config: &Config) -> Option<&String> {
     }
 }
 
-fn handle_activities(msg: &str) -> Option<String> {
-    let activity_regex = Regex::new(r"https?:\/\/www\.strava\.com\/activities\/(\d+)").unwrap();
+fn handle_activities(msg: &str, access_token: &str) -> Option<String> {
+    let activity_regex = Regex::new(r"https?://www\.strava\.com/activities/(\d+)").unwrap();
     for captures in activity_regex.captures_iter(msg) {
-        let activity = Activity::fetch(captures.get(1).unwrap().as_str());
+        let activity = Activity::fetch(captures.get(1).unwrap().as_str(), access_token);
         return match activity {
             Ok(activity) => Some(activity.to_string()),
             Err(e) => {
@@ -55,9 +56,8 @@ struct Activity {
     moving_time: u32,
 }
 impl Activity {
-    fn fetch(id: &str) -> Result<Activity, reqwest::Error> {
-        let strava_token = "";
-        let url = format!("https://www.strava.com/api/v3/activities/{}?access_token={}", id, strava_token);
+    fn fetch(id: &str, access_token: &str) -> Result<Activity, reqwest::Error> {
+        let url = format!("https://www.strava.com/api/v3/activities/{}?access_token={}", id, access_token);
         let mut req = reqwest::get(&url)?;
         println!("{}", req.url());
         req.json()
@@ -66,18 +66,22 @@ impl Activity {
 impl ToString for Activity {
     fn to_string(&self) -> String {
         let distance = (self.distance / 100.0).floor() / 10.0;
-        format!("[STRAVA {sport}] \"{name}\", {distance} km (↑",
+        let pace = (self.moving_time as f64 / (self.distance / 1000.0)).round() as u32;
+        format!("[STRAVA {sport}] \"{name}\", {distance} km (↑{elev}m) in {time} ({pace}/km)",
                 sport = self.sport.to_uppercase(),
                 name = self.name,
-                distance = distance)
+                distance = distance,
+                elev = self.total_elevation_gain.round(),
+                time = format_time(self.moving_time),
+                pace = format_time(pace))
     }
 }
 
-fn handle_segments(msg: &str) -> Option<String> {
+fn handle_segments(msg: &str, access_token: &str) -> Option<String> {
     let segment_regex = Regex::new(r"https?://www\.strava\.com/segments/(\d+)").unwrap();
     for captures in segment_regex.captures_iter(msg) {
         println!("{}", captures.get(1).unwrap().as_str());
-        let segment = Segment::fetch(captures.get(1).unwrap().as_str());
+        let segment = Segment::fetch(captures.get(1).unwrap().as_str(), access_token);
         return match segment {
             Ok(segment) => Some(segment.to_string()),
             Err(e) => {
@@ -103,9 +107,8 @@ struct Segment {
     country: String,
 }
 impl Segment {
-    fn fetch(id: &str) -> Result<Segment, reqwest::Error> {
-        let strava_token = "";
-        let url = format!("https://www.strava.com/api/v3/segments/{}?access_token={}", id, strava_token);
+    fn fetch(id: &str, access_token: &str) -> Result<Segment, reqwest::Error> {
+        let url = format!("https://www.strava.com/api/v3/segments/{}?access_token={}", id, access_token);
         let mut req = reqwest::get(&url)?;
         println!("{}", req.url());
         req.json()
@@ -128,6 +131,17 @@ impl ToString for Segment {
                 city = self.city,
                 state = state,
                 country = self.country)
+    }
+}
+
+fn format_time(seconds: u32) -> String {
+    let hours = (seconds as f64 / 3600.0).floor();
+    let minutes = ((seconds % 3600) as f64 / 60.0).floor();
+    let seconds = seconds % 60;
+    if hours == 0.0 {
+        return format!("{}:{:02}", minutes, seconds);
+    } else {
+        return format!("{}:{:02}:{:02}", hours, minutes, seconds);
     }
 }
 
