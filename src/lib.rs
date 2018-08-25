526fd0ddabf982b625afb3998465dece6be2e05e
Ward Muylaert <ward.muylaert@gmail.com>
2018-08-25 12:55:28 +0200

    Add some simple IRC formatting capability
    Not sure on the API I want, so for now it's just a simpler way to get a
    hold of the different special characters. Seems good enough.

--- src/lib.rs
+++ src/lib.rs
@@ -29,4 +29,71 @@ pub mod plugins {
     pub mod calc;
 
     pub mod nickname;
+
+    pub mod formatting {
+        use std::fmt;
+
+        pub enum IrcFormat {
+            Bold,
+            Normal,
+            Underline,
+            Italic,
+            ForegroundColour(IrcColour),
+            BackgroundColour(IrcColour, IrcColour),
+        }
+        pub enum IrcColour {
+            White,
+            Black,
+            Navy,
+            Green,
+            Red,
+            Brown,
+            Purple,
+            Olive,
+            Yellow,
+            LightGreen,
+            Teal,
+            Cyan,
+            Blue,
+            Pink,
+            Gray,
+            LightGray,
+        }
+        impl fmt::Display for IrcFormat {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                match self {
+                    IrcFormat::Bold => write!(f, "\x02"),
+                    IrcFormat::Normal => write!(f, "\x0F"),
+                    IrcFormat::Underline => write!(f, "\x1F"),
+                    IrcFormat::Italic => write!(f, "\x1D"),
+                    IrcFormat::ForegroundColour(colour) => write!(f, "\x03{}", colour),
+                    IrcFormat::BackgroundColour(text_colour, back_colour) => {
+                        write!(f, "\x03{},{}", text_colour, back_colour)
+                    }
+                }
+            }
+        }
+        impl fmt::Display for IrcColour {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                match self {
+                    IrcColour::White => write!(f, "00"),
+                    IrcColour::Black => write!(f, "01"),
+                    IrcColour::Navy => write!(f, "02"),
+                    IrcColour::Green => write!(f, "03"),
+                    IrcColour::Red => write!(f, "04"),
+                    IrcColour::Brown => write!(f, "05"),
+                    IrcColour::Purple => write!(f, "06"),
+                    IrcColour::Olive => write!(f, "07"),
+                    IrcColour::Yellow => write!(f, "08"),
+                    IrcColour::LightGreen => write!(f, "09"),
+                    IrcColour::Teal => write!(f, "10"),
+                    IrcColour::Cyan => write!(f, "11"),
+                    IrcColour::Blue => write!(f, "12"),
+                    IrcColour::Pink => write!(f, "13"),
+                    IrcColour::Gray => write!(f, "14"),
+                    IrcColour::LightGray => write!(f, "15"),
+                }
+            }
+        }
+    }
 }
