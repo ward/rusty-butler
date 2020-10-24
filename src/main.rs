8ecd427f6a3f700aeface2afb0148a047f7a75b4
Ward Muylaert <ward.muylaert@gmail.com>
2020-10-24 17:25:13 +0200

    v0.3.0. Bump cause of the change with !help

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use clap::{App, Arg};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = App::new("rusty-butler")
-        .version("0.2.0")
+        .version("0.3.0")
         .author("Ward Muylaert")
         .about("An IRC bot. The world needed more of those.")
         .arg(
